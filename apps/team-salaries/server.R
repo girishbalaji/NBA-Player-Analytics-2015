482cd22106ac2b2235d705a90991fb517d920077
canishka18 <canishka18@yahoo.com>
2016-11-27 08:26:34 -0800

    Completed team-salaries app - 1.0

---
+++ apps/team-salaries/server.R
@@ -0,0 +1,49 @@
+server <- function(input, output) {
+  
+  output$stats_plot <- renderPlot({
+    # Read in the clean team salaries data from csv
+    stat_csv = read.csv("data/cleandata/team-salaries.csv")
+    
+    # Variables to store the inputs from the UI
+    statistic = input$statistics
+    ordering = input$order
+    
+    # Vector (Dictionary to access statistics)
+    stat_dict = c(2:10)
+    names(stat_dict) = c("Total Payroll", "Minimum Salary",
+                         "Maximum Salary", "First Quartile",
+                         "Median", "Third Quartile",
+                         "Average", "Interquartile Range",
+                         "Standard Deviation")
+    
+    
+    clean_stat_name = c("Total Payroll", "Minimum Salary",
+                        "Maximum Salary", "First Quartile",
+                        "Median", "Third Quartile",
+                        "Average", "Interquartile Range",
+                        "Standard Deviation")
+    
+    # Accessing the required statistics from the csv
+    current_stat_column = stat_dict[statistic]
+    current_stat = stat_csv[current_stat_column]
+    
+    # Creating data frame
+    df <- data.frame(Teams = stat_csv[1],
+                     Stat = current_stat)
+    
+    # Conditional to consider ordering
+    if(ordering == "Ascending") {
+      xorder= paste0("reorder(team_name, ", colnames(df[2]),")")
+    } else {
+      xorder= paste0("reorder(team_name, -", colnames(df[2]),")")
+    }
+    
+    # Plot the requested statistics
+    ggplot(df, aes_string(x = xorder, y = colnames(df[2]))) +
+      geom_bar(stat = "identity") +
+      coord_flip() + scale_x_discrete(name = "Team Name") +
+      scale_y_continuous(name = clean_stat_name[current_stat_column - 1]) +
+      ggtitle(paste0(clean_stat_name[current_stat_column - 1]," per NBA Team"))
+    
+  })
+}
---
+++ apps/team-salaries/ui.R
@@ -0,0 +1,31 @@
+ui <- fluidPage(
+  #Title for page
+  titlePanel("Statistics Per NBA Team"),
+  
+  #Sidebar to control the input values:
+  #   Type of Statistic
+  #   Ordering
+  sidebarLayout(
+    sidebarPanel(
+      # Type of Statistic
+      selectInput("statistics",
+                  label = "Chosen Statistic:",
+                  c("Total Payroll", "Minimum Salary",
+                    "Maximum Salary", "First Quartile",
+                    "Median", "Third Quartile",
+                    "Average", "Interquartile Range",
+                    "Standard Deviation"),
+                  selected = "Average"),
+      # Ordering
+      radioButtons("order",
+                   label = "Ordering",
+                   c("Ascending", "Descending"))
+
+    ),
+    # Main Panel to visualise the statistics on a 
+    # plot.
+    mainPanel(
+      plotOutput("stats_plot")
+    )
+  )
+)
