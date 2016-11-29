dccb27eb3fd1488bac00e16b4be952d803e8c3fc
canishka18 <canishka18@yahoo.com>
2016-11-28 16:20:01 -0800

    Second Shiny App Working: requires comments and cleaning

---
+++ apps/stat-salaries/ui.R
@@ -0,0 +1,40 @@
+ui <- fluidPage(
+  #Title for page
+  titlePanel("EFF - Statistics - Salary in NBA"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      selectInput("x_statistic",
+                  label = "Chosen X-axis Statistic:",
+                  c("Total Points", "Total Rebounds",
+                    "Assists", "Steals",
+                    "Blocks", "Missed Field Goals",
+                    "Missed Free Throws", "Turnovers",
+                    "Games Played", "Efficiency Index",
+                    "Salary"), 
+                  selected = "Efficiency Index"),
+      
+      selectInput("y_statistic",
+                  label = "Chosen Y-axis Statistic:",
+                  c("Total Points", "Total Rebounds",
+                    "Assists", "Steals",
+                    "Blocks", "Missed Field Goals",
+                    "Missed Free Throws", "Turnovers",
+                    "Games Played", "Efficiency Index",
+                    "Salary"), 
+                  selected = "Salary"),
+      # Sorting
+      radioButtons("sort",
+                   label = "Sort by position?",
+                   c("Yes", "No"))
+      
+    ),
+    # Main Panel to visualise the statistics on a 
+    # plot.
+    mainPanel(
+      plotOutput("player_stats_plot"),
+      textOutput("correlationText")
+    )
+  )
+)
